88dc6c381f1ecd94bd1a8a8c17529afd56e1947a
Alexey Romanov <alexey.v.romanov@gmail.com>
2015-03-16 14:03:58 +0300

    Print PATH for debugging

--- pcg/src/test/java/com/github/alexeyr/pcg/Pcg32CComparisonTest.java
+++ pcg/src/test/java/com/github/alexeyr/pcg/Pcg32CComparisonTest.java
@@ -25,7 +25,10 @@ public class Pcg32CComparisonTest {
     @BeforeClass
     public static void compileC() throws InterruptedException {
         try {
-            Process process = new ProcessBuilder("/bin/sh", "-c", "make").directory(cCodeDir).redirectErrorStream(true).start();
+            ProcessBuilder pb = new ProcessBuilder("make").directory(cCodeDir).redirectErrorStream(true);
+            String str = String.format("PATH in System.getEnv: %s\nPATH in pb.environment: %s", System.getenv("PATH"), pb.environment().get("PATH"));
+            System.out.println(str);
+            Process process = pb.start();
             int exitCode = process.waitFor();
             if (exitCode != 0) {
                 try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
